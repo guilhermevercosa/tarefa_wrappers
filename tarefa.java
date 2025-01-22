0988e5c91c9a29e8f60153870a511430f62e1e36
Guilherme Dias Verçosa <guilhermediasvercosa@gmail.com>
2025-01-22 10:17:13 -0300

    Criando a converção (Primitivo - Wrapper)

--- tarefa.java
+++ tarefa.java
@@ -1,3 +1,10 @@
 public class tarefa {
+    public static void main(String[] args) {
+        System.out.print("Digite um número inteiro: ");
+        int num1 = Integer.parseInt(System.console().readLine());
+        System.out.println("Valor como tipo primitivo: " + num1);
 
+        Integer numWrapper = Integer.valueOf(num1);
+        System.out.println("Valor como tipo wrapper (Integer): " + numWrapper);
+    }
 }
